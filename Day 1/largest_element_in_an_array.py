6167c83d9457dace287be6e91ce2bac810d2ad96
pragatij17 <pj.pragatijain09@gmail.com>
2021-05-01 17:49:40 +0530

    Added: Day 1 code

---
+++ Day 1/largest_element_in_an_array.py
@@ -0,0 +1,11 @@
+def largest_num(array:list)->int:
+    largest_no=-99999
+    largest_index=0
+    for index in range(0,len(array)):
+        if(largest_no<array[index]):
+            largest_no=array[index]
+            largest_index=index
+    return largest_no
+
+array=list(map(int,input().split()))
+print(largest_num(array))
